1ce09bbddb4e74750cfc3c29035f56887e97961d
mitry1974 <36993758+mitry1974@users.noreply.github.com>
2018-08-24 09:46:45 +0300

    3 07 add junit

--- src/test/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
+++ src/test/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
@@ -22,8 +22,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-import static ru.javawebinar.topjava.repository.mock.InMemoryUserRepositoryImpl.ADMIN_ID;
-import static ru.javawebinar.topjava.repository.mock.InMemoryUserRepositoryImpl.USER_ID;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @Repository
 public class InMemoryMealRepositoryImpl implements MealRepository {
@@ -61,7 +61,7 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
     public void preDestroy() {
         log.info("+++ PreDestroy");
     }
-
+    
     @Override
     public boolean delete(int id, int userId) {
         Map<Integer, Meal> meals = repository.get(userId);
