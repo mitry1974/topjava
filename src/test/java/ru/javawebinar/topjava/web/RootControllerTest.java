4939169d60b6aff38b981a3afe77652383059f3f
mt <tomitry>
2018-10-06 20:12:31 +0300

    HW07, 1 Controllers tests added -- RootControllerTest.testMeals for the meals.jsp -- ResourceControllerTest for the style.css

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,10 +2,14 @@ package ru.javawebinar.topjava.web;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Month;
+
+import static java.time.LocalDateTime.of;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.MEAL1_ID;
 import static ru.javawebinar.topjava.UserTestData.USER;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
@@ -25,5 +29,35 @@ class RootControllerTest extends AbstractControllerTest {
                                 hasProperty("name", is(USER.getName()))
                         )
                 )));
+    }
+
+    @Test
+    void testMeals() throws Exception {
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", hasSize(6)))
+                .andExpect(model().attribute("meals",hasItem(allOf(hasProperty("id", is(MEAL1_ID)),
+                        hasProperty("dateTime", is(of(2015, Month.MAY, 30, 10, 0))),
+                        hasProperty("description", is("Завтрак"))))))
+                .andExpect(model().attribute("meals",hasItem(allOf(hasProperty("id", is(MEAL1_ID + 1)),
+                        hasProperty("dateTime", is(of(2015, Month.MAY, 30, 13, 0))),
+                        hasProperty("description", is("Обед"))))))
+                .andExpect(model().attribute("meals",hasItem(allOf(hasProperty("id", is(MEAL1_ID + 2)),
+                        hasProperty("dateTime", is(of(2015, Month.MAY, 30, 20, 0))),
+                        hasProperty("description", is("Ужин"))))))
+                .andExpect(model().attribute("meals",hasItem(allOf(hasProperty("id", is(MEAL1_ID + 3)),
+                        hasProperty("dateTime", is(of(2015, Month.MAY, 31, 10, 0))),
+                        hasProperty("description", is("Завтрак"))))))
+                .andExpect(model().attribute("meals",hasItem(allOf(hasProperty("id", is(MEAL1_ID + 4)),
+                        hasProperty("dateTime", is(of(2015, Month.MAY, 31, 13, 0))),
+                        hasProperty("description", is("Обед"))))))
+                .andExpect( model().attribute("meals", hasItem(allOf(hasProperty("id", is(MEAL1_ID + 5)),
+                        hasProperty("dateTime", is(of(2015, Month.MAY, 31, 20, 0))),
+                        hasProperty("description", is("Ужин"))))
+                        ));
+
     }
 }
